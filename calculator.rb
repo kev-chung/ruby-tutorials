bb85eefc6e0ced8e92ceef57dafe9c7775c60c16
kev-chung <k.chung@outlook.com>
2020-08-26 16:44:21 -0400

    Added more user choices to calculator program

--- calculator.rb
+++ calculator.rb
@@ -6,11 +6,22 @@ puts "Enter the first number:"
 num_1 = gets.chomp
 puts "Enter the second number:"
 num_2 = gets.chomp
+puts "What do you want to do?"
+puts "Enter 1 for multiplication, 2 for division, 3 for subtraction, 4 for addition, or 5 for modulo:"
+choice = gets.chomp
 
-puts "The first number multiplied by the second number is #{num_1.to_f * num_2.to_f}"
-puts "The first number divided by the second number is #{num_1.to_f / num_2.to_f}"
-puts "The first number subtracted from the second number is #{num_2.to_f - num_1.to_f}"
-puts "The first number added to the second number is #{num_1.to_f + num_2.to_f}"
-puts "The first number modulo the second number is #{num_1.to_f % num_2.to_f}"
+if choice == "1"
+    puts "The first number multiplied by the second number is #{num_1.to_f * num_2.to_f}"
+elsif choice == "2"
+    puts "The first number divided by the second number is #{num_1.to_f / num_2.to_f}"
+elsif choice == "3"
+    puts "The first number subtracted from the second number is #{num_2.to_f - num_1.to_f}"
+elsif choice == "4"
+    puts "The first number added to the second number is #{num_1.to_f + num_2.to_f}"
+elsif choice == "5"
+    puts "The first number modulo the second number is #{num_1.to_f % num_2.to_f}"
+else
+    puts "Invalid input"
+end
 
 
