e53057aace55048a854d52a3df1815a6a06d0ae6
kev-chung <k.chung@outlook.com>
2020-08-26 16:23:44 -0400

    Added a simple calculator program

--- analyzer.rb
+++ analyzer.rb
@@ -6,5 +6,5 @@ last_name = gets.chomp
 full_name = "#{first_name} #{last_name}"
 
 puts "Your full name is #{full_name}"
-puts "Your full name reversed is " + full_name.reverse
-puts "Your name has " + (first_name.length + last_name.length).to_s + " characters in it"
+puts "Your full name reversed is #{full_name.reverse}"
+puts "Your name has #{first_name.length + last_name.length} characters in it"
